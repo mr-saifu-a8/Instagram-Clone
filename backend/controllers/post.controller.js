7e1b132c5ca4e3c74845011f174a6fd2c0bd10f1
mrsaifu08 <saifstack25@gmail.com>
2026-03-28 10:27:33 +0530

    feat: implement like functionality with like model and route

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -2,6 +2,7 @@ const postModel = require("../models/post.model");
 const ImageKit = require("@imagekit/nodejs");
 const { toFile } = require("@imagekit/nodejs");
 const jwt = require("jsonwebtoken");
+const likeModel = require('../models/like.model')
 
 const imagekit = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY, // This is the default and can be omitted
@@ -60,4 +61,25 @@ const getPostDetails = async (req, res) => {
   });
 };
 
-module.exports = { postCreating, getPostController, getPostDetails };
+const likePost = async (req, res) => {
+  const username = req.user.username
+  const postId = req.params.postId
+
+  const post = await postModel.findById(postId)
+
+  if (!post) {
+    return res.status(404).json({message: "Post not found"})
+  }
+
+  const like = likeModel.create({
+    post: postId,
+    user: username
+  })
+
+  res.status(200).json({
+    message: "Post like successfully",
+    like
+  })
+}
+
+module.exports = { postCreating, getPostController, getPostDetails, likePost };
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -8,5 +8,6 @@ const identifyUser = require('../middlewares/auth.middleware.js')
 postRouter.post("/", upload.single("image"), identifyUser, postController.postCreating);
 postRouter.get("/",identifyUser, postController.getPostController);
 postRouter.get("/details/:postId", identifyUser, postController.getPostDetails);
+postRouter.post('/like/:postId', identifyUser, postController.likePost)
 
 module.exports = postRouter;
