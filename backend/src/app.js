383687f7143692f7632b2fd37f66118a73033a43
mrsaifu08 <saifstack25@gmail.com>
2026-03-28 12:30:23 +0530

    feat: add CORS support and axios for API requests in frontend registration

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,18 @@
-const express = require('express')
-const app = express()
-const cookieParser = require('cookie-parser')
-const authRouter = require("../routes/auth.routes")
-const postRouter = require('../routes/post.routes')
-const userRouter = require('../routes/user.routes')
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const authRouter = require("../routes/auth.routes");
+const postRouter = require("../routes/post.routes");
+const userRouter = require("../routes/user.routes");
+const cors = require("cors");
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-app.use("/api/auth", authRouter)
-app.use('/api/posts', postRouter)
-app.use('/api/users', userRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postRouter);
+app.use("/api/users", userRouter);
 
-module.exports = app
+module.exports = app;
--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,27 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/form.scss";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post(
+        "http://localhost:3000/api/auth/register",
+        {
+          username,
+          email,
+          password,
+        },
+        { withCredentials: true },
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
   return (
     <main className="auth-page">
       <div className="form-container">
         <h1>Create Account</h1>
         <p className="subtitle">Join us and get started 🚀</p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-group">
-            <input type="text" name="username" required />
+            <input
+              onInput={(e) => {
+                setUsername(e.target.value);
+              }}
+              type="text"
+              name="username"
+              required
+            />
             <label>Username</label>
           </div>
 
           <div className="input-group">
-            <input type="email" name="email" required />
+            <input
+              onInput={(e) => {
+                setEmail(e.target.value);
+              }}
+              type="email"
+              name="email"
+              required
+            />
             <label>Email</label>
           </div>
 
           <div className="input-group">
-            <input type="password" name="password" required />
+            <input
+              onInput={(e) => {
+                setPassword(e.target.value);
+              }}
+              type="password"
+              name="password"
+              required
+            />
             <label>Password</label>
           </div>
 
