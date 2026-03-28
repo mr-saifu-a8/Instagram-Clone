8a17347005cbfe6b9deebb8897974c634a1df481
mrsaifu08 <saifstack25@gmail.com>
2026-03-28 12:55:15 +0530

    feat: implement login functionality with form handling and API integration

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,22 +1,41 @@
-import React from "react";
+
 import "../style/form.scss";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+
+
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    axios.post("http://localhost:3000/api/auth/login", {
+      username,
+      password
+    }, {withCredentials: true}).then((res) => {
+      console.log(res.data);
+    });
+  };
   return (
     <main className="auth-page">
       <div className="form-container">
         <h1>Welcome Back</h1>
         <p className="subtitle">Login to continue 🔐</p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-group">
-            <input type="text" name="username" required />
+            <input onInput={(e)=> {setUsername(e.target.value)}} type="text" name="username" required />
             <label>Username</label>
           </div>
 
           <div className="input-group">
-            <input type="password" name="password" required />
+            <input onInput={(e) => {
+              setPassword(e.target.value)
+            }} type="password" name="password" required />
             <label>Password</label>
           </div>
 
